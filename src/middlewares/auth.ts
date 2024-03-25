6b1769886a1cd6f72175360f87109443e5c1e478
Off developer <offdeveloper0@gmail.com>
2024-03-25 15:28:24 -0300

    fix: api key

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,18 +1,17 @@
 import { Response, NextFunction } from 'express';
 import { ExtendedRequest } from '../../@types';
-
+import bcrypt from 'bcrypt';
 export default function auth(request: ExtendedRequest, response: Response, next: NextFunction) {
 
-    const authHeader = request.headers['x-api-key'];
+    const authHeader: any = request.headers['x-api-key'];
 
     if (!authHeader) {
         return response.status(401).json({ error: "chave de api não encontrada" });
     }
-
     try {
         const apiKey = process.env.API_KEY as string;
-
-        if (apiKey !== authHeader) {
+        const result = bcrypt.compare(apiKey, authHeader)
+        if (!result) {
             return response.status(401).json({ error: "chave invalida" });
         }
 
